f73bcc448636176205e025ff7f3684286346b9e5
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2024-05-03 10:13:46 -0400

    Update addons.py

--- addons.py
+++ addons.py
@@ -1,9 +1,15 @@
 del install_addons
 del install_addonsbeta
 
+
+def complete_install():
+  print("Addons installed successfully!")
+
 def newinstall_addons():
   print("Addons already installed!")
 
 new_responses = {"Install addons":newinstall_addons, "Install addons beta":newinstall_addons}
 
 responses = dict(responses, **new_responses)
+
+complete_install()
