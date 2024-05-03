19be7cae86d6fb19efe2c03e2c0f2507cc12c815
PPPDUD <107440101+PPPDUD@users.noreply.github.com>
2024-05-03 10:47:05 -0400

    Update addons.py

--- addons.py
+++ addons.py
@@ -2,6 +2,16 @@ del install_addons
 del install_addonsbeta
 
 
+# Standard I/O for addons; use instead of input() and print()
+def addon_print(string=""):
+  print(string)
+
+def addon_input(prompt=""):
+  return input(prompt)
+
+def addon_chatbot(prompt);
+  chatbot(prompt)
+
 def complete_install():
   print("Addons installed successfully!")
 
